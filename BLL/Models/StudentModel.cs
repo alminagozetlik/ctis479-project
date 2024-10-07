a4891ce7d23a596b471b220737cacba9d5f5114b
Almina <Almina@DESKTOP-DPBFNMA>
2024-10-07 17:57:39 +0300

    Add project files.

---
+++ BLL/Models/StudentModel.cs
@@ -0,0 +1,29 @@
+﻿#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BLL.DAL;
+
+namespace BLL.Models
+{
+    public class StudentModel
+    {
+        public Student Record { get; }
+
+        public int Id => Record.Id;
+
+        public string Name => Record.Name;
+
+        public string Surname => Record.Surname;
+
+        public DateTime? BirthDate => Record.BirthDate;
+
+        public decimal? Gpa => Record.Gpa;
+
+
+    }
+}
